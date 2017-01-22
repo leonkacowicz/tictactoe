e391e1a668c8e6f4073f821e52bda845fa6af5c0
Leon Kacowicz <leon.kacowicz@gmail.com>
2017-01-22 19:41:47 -0200

    Board.setCellState tests

--- src/test/java/io/github/leonkacowicz/tictactoe/core/BoardTest.java
+++ src/test/java/io/github/leonkacowicz/tictactoe/core/BoardTest.java
@@ -21,4 +21,47 @@ public class BoardTest {
         Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
         Assert.assertEquals(CellState.BLANK, board.cellStates[1][1]);
     }
+
+
+    /**
+     * Given: a board
+     * When: I set a cell's state
+     * Then: The state is set to the correct value
+     * @throws Exception
+     */
+    @Test
+    public void test_setCellState() throws Exception {
+
+        Board board = new Board(2);
+
+        board.setCellState(0, 0, CellState.CIRCLE);
+        Assert.assertEquals(CellState.CIRCLE, board.cellStates[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[1][1]);
+
+        board.setCellState(0, 0, CellState.CROSS);
+        Assert.assertEquals(CellState.CROSS, board.cellStates[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[1][1]);
+
+        board.setCellState(0, 0, CellState.BLANK);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[1][1]);
+
+        board.setCellState(1, 1, CellState.CIRCLE);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
+        Assert.assertEquals(CellState.CIRCLE, board.cellStates[1][1]);
+
+        board.setCellState(0, 1, CellState.CROSS);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[0][0]);
+        Assert.assertEquals(CellState.CROSS, board.cellStates[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
+        Assert.assertEquals(CellState.CIRCLE, board.cellStates[1][1]);
+    }
 }
