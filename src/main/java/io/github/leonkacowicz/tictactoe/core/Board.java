d187ab30f9d9111f5694d3d6cbecdad4cd87dfe4
Leon Kacowicz <leon.kacowicz@gmail.com>
2017-01-22 20:21:33 -0200

    Refactoring and Board::getBoardState method body

--- src/main/java/io/github/leonkacowicz/tictactoe/core/Board.java
+++ src/main/java/io/github/leonkacowicz/tictactoe/core/Board.java
@@ -1,35 +1,119 @@
 package io.github.leonkacowicz.tictactoe.core;
 
+import static io.github.leonkacowicz.tictactoe.core.BoardState.CIRCLE_WINS;
+import static io.github.leonkacowicz.tictactoe.core.BoardState.CROSS_WINS;
+import static io.github.leonkacowicz.tictactoe.core.BoardState.DRAW;
+import static io.github.leonkacowicz.tictactoe.core.CellState.*;
+
 public class Board {
-    protected CellState[][] cellStates;
+    protected CellState[][] board;
 
     public Board() {
         this(3);
     }
 
     public Board(int n) {
-        cellStates = new CellState[n][n];
+        board = new CellState[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                cellStates[i][j] = CellState.BLANK;
+                board[i][j] = CellState.BLANK;
             }
         }
     }
 
     public void setCellState(int row, int column, CellState cellState) {
-        cellStates[row][column] = cellState;
+        board[row][column] = cellState;
     }
 
     public CellState getCellState(int row, int column) {
-        return cellStates[row][column];
+        return board[row][column];
     }
 
     public int size() {
-        return cellStates.length;
+        return board.length;
     }
 
     public BoardState getBoardState() {
+
+        final int n = board.length;
+        int impossibleRows = 0;
+        int impossibleColumns = 0;
+        int impossibleDiag = 0;
+
+        for (int row = 0; row < n; row++) {
+            boolean allCircle = true;
+            boolean hasCircle = false;
+            boolean allCross = true;
+            boolean hasCross = false;
+
+            for (int col = 0; col < n; col++) {
+                allCircle = allCircle && (board[row][col] == CIRCLE);
+                allCross = allCross && (board[row][col] == CROSS);
+                hasCircle = hasCircle || (board[row][col] == CIRCLE);
+                hasCross = hasCross || (board[row][col] == CROSS);
+            }
+
+            if (allCircle) return CIRCLE_WINS;
+            if (allCross) return CROSS_WINS;
+            if (hasCircle && hasCross) ++impossibleRows;
+        }
+
+        for (int col = 0; col < n; col++) {
+            boolean allCircle = true;
+            boolean hasCircle = false;
+            boolean allCross = true;
+            boolean hasCross = false;
+
+            for (int row = 0; row < n; row++) {
+                allCircle = allCircle && (board[row][col] == CIRCLE);
+                allCross = allCross && (board[row][col] == CROSS);
+                hasCircle = hasCircle || (board[row][col] == CIRCLE);
+                hasCross = hasCross || (board[row][col] == CROSS);
+            }
+
+            if (allCircle) return CIRCLE_WINS;
+            if (allCross) return CROSS_WINS;
+            if (hasCircle && hasCross) ++impossibleColumns;
+        }
+
+        // main diagonal
+        boolean allCircle = true;
+        boolean hasCircle = false;
+        boolean allCross = true;
+        boolean hasCross = false;
+
+        for (int i = 0; i < n; i++) {
+            allCircle = allCircle && (board[i][i] == CIRCLE);
+            allCross = allCross && (board[i][i] == CROSS);
+            hasCircle = hasCircle || (board[i][i] == CIRCLE);
+            hasCross = hasCross || (board[i][i] == CROSS);
+        }
+
+        if (allCircle) return CIRCLE_WINS;
+        if (allCross) return CROSS_WINS;
+        if (hasCircle && hasCross) ++impossibleDiag;
+
+
+        // main diagonal
+        allCircle = true;
+        hasCircle = false;
+        allCross = true;
+        hasCross = false;
+
+        for (int i = 0; i < n; i++) {
+            allCircle = allCircle && (board[i][i] == CIRCLE);
+            allCross = allCross && (board[i][i] == CROSS);
+            hasCircle = hasCircle || (board[i][i] == CIRCLE);
+            hasCross = hasCross || (board[i][i] == CROSS);
+        }
+
+        if (allCircle) return CIRCLE_WINS;
+        if (allCross) return CROSS_WINS;
+        if (hasCircle && hasCross) ++impossibleDiag;
+
+
+        if (impossibleRows == n && impossibleColumns == n && impossibleDiag == 2) return DRAW;
         return BoardState.NOT_FINISHED;
     }
 }
--- src/test/java/io/github/leonkacowicz/tictactoe/core/BoardTest.java
+++ src/test/java/io/github/leonkacowicz/tictactoe/core/BoardTest.java
@@ -16,10 +16,10 @@ public class BoardTest {
 
         Board board = new Board(2);
 
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][1]);
     }
 
 
@@ -35,34 +35,34 @@ public class BoardTest {
         Board board = new Board(2);
 
         board.setCellState(0, 0, CellState.CIRCLE);
-        Assert.assertEquals(CellState.CIRCLE, board.cellStates[0][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][1]);
+        Assert.assertEquals(CellState.CIRCLE, board.board[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][1]);
 
         board.setCellState(0, 0, CellState.CROSS);
-        Assert.assertEquals(CellState.CROSS, board.cellStates[0][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][1]);
+        Assert.assertEquals(CellState.CROSS, board.board[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][1]);
 
         board.setCellState(0, 0, CellState.BLANK);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][1]);
 
         board.setCellState(1, 1, CellState.CIRCLE);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][0]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][1]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
-        Assert.assertEquals(CellState.CIRCLE, board.cellStates[1][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][0]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][0]);
+        Assert.assertEquals(CellState.CIRCLE, board.board[1][1]);
 
         board.setCellState(0, 1, CellState.CROSS);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[0][0]);
-        Assert.assertEquals(CellState.CROSS, board.cellStates[0][1]);
-        Assert.assertEquals(CellState.BLANK, board.cellStates[1][0]);
-        Assert.assertEquals(CellState.CIRCLE, board.cellStates[1][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[0][0]);
+        Assert.assertEquals(CellState.CROSS, board.board[0][1]);
+        Assert.assertEquals(CellState.BLANK, board.board[1][0]);
+        Assert.assertEquals(CellState.CIRCLE, board.board[1][1]);
     }
 
     /**
@@ -75,10 +75,10 @@ public class BoardTest {
     public void test_getCellState() throws Exception {
 
         Board board = new Board(2);
-        board.cellStates[0][0] = CellState.CIRCLE;
-        board.cellStates[0][1] = CellState.CROSS;
-        board.cellStates[1][0] = CellState.BLANK;
-        board.cellStates[1][1] = CellState.CIRCLE;
+        board.board[0][0] = CellState.CIRCLE;
+        board.board[0][1] = CellState.CROSS;
+        board.board[1][0] = CellState.BLANK;
+        board.board[1][1] = CellState.CIRCLE;
 
         Assert.assertEquals(CellState.CIRCLE, board.getCellState(0, 0));
         Assert.assertEquals(CellState.CROSS, board.getCellState(0, 1));
@@ -100,9 +100,9 @@ public class BoardTest {
         Assert.assertEquals(BoardState.NOT_FINISHED, board.getBoardState());
 
         board = new Board(3);
-        board.cellStates[0][0] = CellState.CIRCLE;
-        board.cellStates[1][1] = CellState.CIRCLE;
-        board.cellStates[2][2] = CellState.CIRCLE;
+        board.board[0][0] = CellState.CIRCLE;
+        board.board[1][1] = CellState.CIRCLE;
+        board.board[2][2] = CellState.CIRCLE;
         Assert.assertEquals(BoardState.CIRCLE_WINS, board.getBoardState());
     }
 }
